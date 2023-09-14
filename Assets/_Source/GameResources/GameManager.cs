35efdace06a4838b9434bc5f5e9feea5391c345b
MShpiz <reg@panfilova.com>
2023-09-14 22:00:34 +0300

    feat: Added clickable buttons which increace amount of resources

--- Assets/_Source/GameResources/GameManager.cs
+++ Assets/_Source/GameResources/GameManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using System;
 
 namespace GameResources
 {
@@ -10,11 +12,20 @@ namespace GameResources
     {
         public ResourceBank _bank = new ResourceBank();
         [SerializeField]  public TMP_Text simpleText;
+        [SerializeField]  public Button[] resourceButtons = new Button[Enum.GetValues(typeof(GameResource)).Length];
         private void Awake()
         {
             _bank.ChangeResource(GameResource.Humans, 10);
             _bank.ChangeResource(GameResource.Food, 5);
             _bank.ChangeResource(GameResource.Wood, 5);
+
+            int btn = 0;
+            foreach (GameResource res in Enum.GetValues(typeof(GameResource)))
+            {
+                resourceButtons[btn]?.onClick.AddListener(delegate () { _bank.ChangeResource(res, _bank.GetResource(res).Value + 1); });
+                btn++;
+            }
+
         }
         void Start()
         {
@@ -29,6 +40,14 @@ namespace GameResources
                 $"Wood {_bank.GetResource(GameResource.Wood).Value} " +
                 $"Stone {_bank.GetResource(GameResource.Stone).Value} " +
                 $"Gold {_bank.GetResource(GameResource.Gold).Value}");
+        }
+
+        private void OnDisable()
+        {
+            for (int i = 0; i < resourceButtons.Length; i++)
+            {
+                resourceButtons[i].onClick.RemoveAllListeners();
+            }
         }
     }
 }
