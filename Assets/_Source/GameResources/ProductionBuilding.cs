7bc7b661e6094e496e82b6ea8f62440a9bdc52b8
MShpiz <reg@panfilova.com>
2023-09-15 19:24:39 +0300

    feat: added delay for buttons

--- Assets/_Source/GameResources/ProductionBuilding.cs
+++ Assets/_Source/GameResources/ProductionBuilding.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System;
 
 namespace GameResources {
     public class ProductionBuilding : MonoBehaviour
     {
         // Start is called before the first frame update
         [SerializeField] public GameResource resource;
+        private uint _productionTime = 10;
 
         private Button _btn;
         private GameManager _manager;
+        private Event onClicked;
         void Awake()
         {
             _btn = GameObject.Find(this.name).GetComponent<Button>();
             _manager = GameObject.Find("ResourceVisual").GetComponent<GameManager>();
 
-            _btn.onClick.AddListener(() => _manager._bank.ChangeResource(resource, _manager._bank.GetResource(resource).Value + 1));
+            _btn.onClick.AddListener(ClickTask);
         }
 
         // Update is called once per frame
@@ -29,6 +32,26 @@ namespace GameResources {
         private void OnDestroy()
         {
             _btn.onClick.RemoveAllListeners();
+        }
+
+        private void ClickTask()
+        {
+            if (!_btn.interactable)
+            {
+                return;
+            }
+            _btn.interactable = false;
+            StartCoroutine(MakeResource());
+        }
+
+        private IEnumerator MakeResource()
+        {
+            yield return new WaitForSeconds(_productionTime);
+            _manager._bank.ChangeResource(resource, _manager._bank.GetResource(resource).Value + 1);
+            if (!_btn.interactable)
+            {
+                _btn.interactable = true;
+            }
         }
     }
 }
